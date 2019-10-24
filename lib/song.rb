d8a73c930f4e9f6ca9366eed4b110133a3478bef
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-24 20:02:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,8 @@ class Song
   def self.new_by_filename(filename)
     song = self.new
     # binding.pry
-      MP3Importer.files.each {song.name = filename.split(" - ")[1]
-      song.artist = filename.split(" - ")[0]
+      MP3Importer.files.each {song.name = filename.split(" - ")[1]}
+      MP3Importer.files.each {song.artist = filename.split(" - ")[0]}
   end
 
   # def artist_name=(name)
