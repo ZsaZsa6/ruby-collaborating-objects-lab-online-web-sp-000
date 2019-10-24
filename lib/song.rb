ce507fe5ad498d8914f915f205136bdddbf43e9a
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-24 20:45:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
 
   def self.new_by_filename(filename)
     # binding.pry
-      song = self.new
+      song = Song.new
       song.name = filename.split(" - ")[1]
       song.artist_name = filename.split(" - ")[0]
   end
