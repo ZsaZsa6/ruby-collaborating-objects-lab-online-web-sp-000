fad2910007f490602f4b1a2eae2e12c1779353f5
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-25 01:30:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
   def self.new_by_filename(filename)
       song = Song.new(name)
       # binding.pry
-      Artist.find_or_create_by_name(name){|name| name = filename.split(" - ")[0]}
+      Artist.find_or_create_by_name {|name| name = filename.split(" - ")[0]}
       song.name = filename.split(" - ")[1]
       # song.artist = Artist.add_song(song)
   end
