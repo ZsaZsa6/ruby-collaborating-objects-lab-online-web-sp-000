31c534a255f7a2ba1461736e19d13d21232a4618
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-25 02:17:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ class Song
       # name = Artist.add_song(filename.split(" - ")[1])
 
       song.artist = Artist.find_or_create_by_name(filename.split(" - ")[1])
-      aong.name = (filename.split(" - ")[0])
+      song.name = (filename.split(" - ")[0])
 # our_song_instance.artist = our_artist_instance.
   end
 
