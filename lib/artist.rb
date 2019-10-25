3a9e3fe01c27bb83e9d00fe2533d7dd38259f9fe
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-25 03:46:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,7 +32,7 @@ class Artist
   end
 
   def print_songs
-    Song.all.select {|song| song.artist puts song.name}
+    Song.all.select {|song| song.artist == self}.each { |song| puts song.name } 
 
   end
 end
